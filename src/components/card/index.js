5d5c97e1d9f141f711aa1554dc52f9443c280912
Lucas Iori <liori@aktienow.com>
2020-05-22 11:16:59 -0300

    feat: Customized candidates card layout

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -13,23 +13,49 @@ function Card(props) {
     return (
         <div className="card">
             <div className="card-header">
+                <span class="header-detail red-detail"></span>
+                <span class="header-detail yellow-detail"></span>
+                <span class="header-detail green-detail"></span>
+            </div>
+
+            <div className="card-body">
                 <h3 className="card-name">
                     {name}
                 </h3>
-                <p className="card-position">
-                    {cargo}
-                </p>
-            </div>
-            <div className="card-body">
-                <p className="contact-info lowercase">
-                    <a href={linkedin} target='_blank' rel="noopener noreferrer" ><img className="icon" src={linkedinIcon} alt="Linkedin Icon" /></a>
-                </p>
-                <p className="contact-info lowercase">
-                    <img className="icon" src={emailIcon} alt="Linkedin Icon" /><a href={`mailto:${email}`} >{email}</a>
-                </p>
-                <p className="contact-info">
-                    <img className="icon" src={houseIcon} alt="Linkedin Icon" />{cidade}
-                </p>
+
+                <section class="jobs">
+                    <div class="section-title">
+                        <h4>Cargos</h4>
+                    </div>
+
+                    <p>{cargo}</p>
+                </section>
+
+                <section class="contacts">
+                    <div class="section-title">
+                        <h4>Contatos</h4>
+                    </div>
+
+                    <p className="contact-info linkedin lowercase" title={linkedin}>
+                        <img className="icon" src={linkedinIcon} alt="Linkedin Icon" />
+                        <a href={linkedin} target='_blank' rel="noopener noreferrer" >
+                            {
+                                linkedin && linkedin.replace('https:', '')
+                                    .replace('http:', '')
+                                    .replace('//', '')
+                                    .replace('www.', '')
+                            }
+                        </a>
+                    </p>
+                    <p className="contact-info email lowercase" title={email}>
+                        <img className="icon" src={emailIcon} alt="Linkedin Icon" />
+                        <a href={`mailto:${email}`} >{email}</a>
+                    </p>
+                    <p className="contact-info address" title={cidade}>
+                        <img className="icon" src={houseIcon} alt="Linkedin Icon" />
+                        <span>{cidade}</span>
+                    </p>
+                </section>
             </div>
         </div>
     )
