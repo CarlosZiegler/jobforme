7f49575b7651e468b3dc84f8c0bcac80d9531b86
Lucas Pinheiro <lucaspinheiroifsul@gmail.com>
2020-05-26 11:31:31 -0300

    add absolute imports

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 /* eslint-disable react/jsx-props-no-spreading */
 
-import '../styles/global.css';
+import 'styles/global.css';
 
 export default function App({ Component, pageProps }) {
 	return <Component {...pageProps} />;
