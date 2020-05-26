babe6de3683b4f6a5b98d56a2de2d0f455c9f28a
Lucas Pinheiro <lucaspinheiroifsul@gmail.com>
2020-05-26 11:34:37 -0300

    fix imports and lint

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 /* eslint-disable react/jsx-props-no-spreading */
 
-import 'styles/global.css';
+import '@styles/global.css';
 
 export default function App({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+  return <Component {...pageProps} />;
 }
--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
 export default async function api(url) {
-	const response = await fetch(url);
-	return response.json();
+  const response = await fetch(url);
+  return response.json();
 }
